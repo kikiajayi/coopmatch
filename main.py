db010cd5dd7ac931abc987c3bf240efbd4e12fa9
Kiki Ajayi <kikiajayi09@gmail.com>
2026-02-25 01:31:26 -0500

    updated calculate score function to additionally identify keywords with that are present in job description and not in resume

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from utils import read_file, clean_text, calculate_score
+from utils import read_file, clean_text, analyse
 resume = read_file("resume_sample.txt")
 job = read_file("job_sample.txt")
 
 resume_clean = clean_text(resume)
 job_clean = clean_text(job)
 
-score = calculate_score(resume_clean, job_clean)
-print(score)
+score = analyse(resume_clean, job_clean)
--- utils.py
+++ utils.py
@@ -41,22 +41,61 @@ def clean_text(content):
     content = content.lower().strip()
     return content
 
-def calculate_score(resume, job):
+def analyse(resume, job, n = 20):
     '''
-    Calculates how similar the resume is to the job qualifications
+    Calculates how similar the resume is to the job qualifications and identifies keywords that are present in job description but not resume 
     
     Arg(s):
         resume: resume read from the file 
         job: job description read from the file
+        n: number of keywords
 
     Returns:
-        float: score to show how closely resume and job description match 
+        score: numerical value (float) to show how closely resume and job description match
+        missingWords: list of key words and 
+
     '''
+    applicationWords = {"interns", "intern", "internship",
+                        "co op", "co ops", "coop", "coops", 
+                        "hour", "hours", "hourly", "relocate",
+                        "location", "location", "applicant",
+                        "applicants", "qualification", "qualifications",
+                        "preferred", "experience", "basic", "entry level"}
     files = [resume, job]
-    tfidf = TfidfVectorizer()
+    tfidf = TfidfVectorizer(stop_words="english", ngram_range=(1,2))
     result = tfidf.fit_transform(files)
-    similarity = cosine_similarity(result[0], result[1])
-    score = similarity[0][0]*100
-    return round(score, 2)
+
+    job_vec = result[1]
+
+    similarity = cosine_similarity(result[0], job_vec)
+
+    #find similarity score between resume and job description
+    score = round(similarity[0][0]*100, 2)
+
+    #identifying keywords from job description that are missing from resume
+    keywords = tfidf.get_feature_names_out()
+    values = job_vec.toarray()[0]
+
+    keywordValues = list(zip(keywords, values))
+
+    sortedKeywordValues = sorted(keywordValues, key=lambda x: x[1], reverse=True)
+
+    missingWords = list()
+
+    #filter past words already in resume
+    for keyword, value in sortedKeywordValues:
+        if value < 0.00001:
+            continue
+        if keyword in resume:
+            continue
+        if keyword in applicationWords:
+            continue
+        missingWords.append((keyword, value))
+
+    for i in range(min(n, len(missingWords))):
+        print(missingWords[i])
+
+    return score, missingWords
+   
 
 
